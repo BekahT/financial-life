62274ea9aa11f87777cab8fd51147ea6b529dde8
Rebekah Qu <rebekah.treadwell@gmail.com>
2020-11-29 19:42:10 -0500

    upgrade ng 11

--- src/app/liabilities/liabilities.component.spec.ts
+++ src/app/liabilities/liabilities.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LiabilitiesComponent } from './liabilities.component';
 
@@ -6,7 +6,7 @@ describe('LiabilitiesComponent', () => {
   let component: LiabilitiesComponent;
   let fixture: ComponentFixture<LiabilitiesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LiabilitiesComponent ]
     })
