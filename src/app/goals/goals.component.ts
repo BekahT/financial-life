69638cd6c224701dfbe220c62315450f693a656f
Rebekah Qu <rebekah.treadwell@gmail.com>
2020-12-02 22:15:03 -0500

    Add momentjs, finish goal add form

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from '../shared/services/firebase.service';
 
 import { Goal } from './goal.model';
 import { Asset } from '../assets/asset.model';
 import { Liability } from '../liabilities/liability.model';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
   styleUrls: ['./goals.component.css']
 })
-export class GoalsComponent implements OnInit {
+export class GoalsComponent implements OnInit, OnDestroy {
   newGoalForm = new FormGroup({
     category: new FormControl('', Validators.required),
     source: new FormControl('', Validators.required),
-    amount: new FormControl('', Validators.required),
+    amount: new FormControl('', [Validators.required, Validators.min(1)]),
     completionDate: new FormControl(''),
   });
 
@@ -25,20 +28,56 @@ export class GoalsComponent implements OnInit {
   sources: string[] = [];
   assets: Asset[] = [];
   liabilities: Liability[] = [];
+  selectedLiability: Liability;
+  payoffError: string;
 
   assetsRef = this.dbs.db.collection('assets');
   liabilitesRef = this.dbs.db.collection('liabilities');
+
+  compDest: Subject<any> = new Subject;
 
 
   constructor(private dbs: FirebaseService) { }
 
   ngOnInit(): void {
+    this.newGoalForm.get('category').valueChanges.pipe(
+      takeUntil(this.compDest)
+    ).subscribe((newCategory) => {
+      this.onCategory(newCategory);
+    });
+
+    this.newGoalForm.get('source').valueChanges.pipe(
+      takeUntil(this.compDest)
+    ).subscribe((newSource) => {
+      this.onSource(newSource);
+    });
+
+    this.newGoalForm.get('amount').valueChanges.pipe(
+      takeUntil(this.compDest),
+      debounceTime(1500),
+      distinctUntilChanged()
+    ).subscribe((newAmount) => {
+      this.onAmount(newAmount);
+    });
+
+
   }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this.compDest.next();
+    this.compDest.complete();
+  }
 
   getError(control: string) {
-    return this.newGoalForm.get(control).hasError('required') ? 'Please enter a value' : '';
+    if (this.newGoalForm.get(control).hasError('required')) {
+      return 'Please enter a value';
+    } else if (this.newGoalForm.get(control).hasError('min')) {
+      return 'Please enter an amount greater than 1';
+    } else {
+      return 'Invalid value';
+    }
+
+
   }
 
   submitForm() {
@@ -83,11 +122,30 @@ export class GoalsComponent implements OnInit {
     this.selectedSource = source;
     if(this.selectedCategory === 'Debt Payoff') {
       // Set the completion date to the due date of the debt
-      let selectedLiability = this.liabilities.find(element => element.name === this.selectedSource);
-      if (selectedLiability.dueDate !== "") {
+      this.selectedLiability = this.liabilities.find(element => element.name === this.selectedSource);
+      if (this.selectedLiability.dueDate !== "") {
         this.newGoalForm.patchValue({
-          completionDate: new Date(selectedLiability.dueDate)
+          completionDate: new Date(this.selectedLiability.dueDate)
         });
+      }
+    }
+  }
+
+  onAmount(amount: number) {
+    if(this.newGoalForm.valid && this.newGoalForm.get('completionDate').value) {
+      let completionDate = moment(this.newGoalForm.get('completionDate').value);
+      let today = moment();
+      let monthsReq = this.selectedLiability.balance / amount;
+      let monthsLeft = completionDate.diff(today, 'months');
+
+      if (monthsReq > monthsLeft) {
+        let monthsNeeded = (monthsReq - monthsLeft).toFixed(0);
+        let amountNeeded = (this.selectedLiability.balance / monthsLeft).toFixed(2);
+        this.payoffError = "The debt will not be paid off by the due date with the monthly amount specified. " +
+        "At the current monthly amount, an additional " + monthsNeeded + " months are required. " +
+        "Alternatively, the debt can be paid in time by paying more than $" + amountNeeded + " per month.";
+      } else {
+        this.payoffError = "";
       }
     }
   }
